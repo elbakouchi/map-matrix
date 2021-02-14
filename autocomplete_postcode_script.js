a4ed38cc3bb42267a0eb7779414635421aea1c14
hamza <hamza.bakouchi@gmail.com>
2021-02-14 12:06:14 +0100

    autocomplete css not woking yet

--- autocomplete_postcode_script.js
+++ autocomplete_postcode_script.js
@@ -1,72 +1,75 @@
-jQuery(document).ready(function(){
-  new autoComplete({
-    data: {                              // Data src [Array, Function, Async] | (REQUIRED)
+jQuery(document).ready(function () {
+  window.autoComplete = new autoComplete({
+    data: {
       src: async () => {
-        // User search query
+        const loader = document.querySelector('#loader');
         const postcode = document.querySelector("#autoComplete").value;
-        // Fetch External Data Source api.postcodes.io/postcodes//autocomplete
+        loader.style.display = 'inline-block';
         const source = await fetch(`https://api.postcodes.io/postcodes/${postcode}/autocomplete`);
-        // Format data into JSON
         const data = await source.json();
-        // Return Fetched data
-        const store = 1;
+        loader.style.display = 'none';
         console.log(source, data);
-        return (data.result === null)?['Nothing found']:data.result;
+        return (data.result === null) ? [] : data.result;
       },
-    //  key: ["title"],
-      cache: false
+      cache: true
     },
-    query: {                             // Query Interceptor               | (Optional)
-          manipulate: (query) => {
-            console.debug(query);
-            return query;
-            //return query.replace("pizza", "burger");
-          }
+    query: {
+      manipulate: (query) => {
+        return query;
+      }
     },
     trigger: {
       event: ["input", "focus"],
     },
-    // sort: (a, b) => {                    // Sort rendered results ascendingly | (Optional)
-    //     if (a.match < b.match) return -1;
-    //     if (a.match > b.match) return 1;
-    //     return 0;
-    // },
-    placeHolder: "Enter a postcode...",     // Place Holder text                 | (Optional)
-    selector: "#autoComplete",           // Input field selector              | (Optional)
-    observer: false,                      // Input field observer | (Optional)
-    threshold: 1,                        // Min. Chars length to start Engine | (Optional)
-    debounce: 300,                       // Post duration for engine to start | (Optional)
-    searchEngine: "loose",              // Search Engine type/mode           | (Optional)
-    resultsList: {                       // Rendered results list object      | (Optional)
-        container: source => {
-          console.debug(source);
-            source.setAttribute("id", "postcodes_list");
-        },
-        destination: "#autoComplete",
-        position: "afterend",
-        element: "div"
+    placeHolder: "Enter a postcode...",
+    selector: "#autoComplete",
+    observer: false,
+    threshold: 1,
+    debounce: 300,
+    searchEngine: "loose",
+    resultsList: {
+      container: source => {
+        source.setAttribute("id", "autocomplete_list");
+      },
+      destination: "#autoComplete",
+      position: "afterend",
+      element: "ul",
     },
-    maxResults: 15,                         // Max. number of rendered results | (Optional)
-    highlight: true,                       // Highlight matching results      | (Optional)
-    resultItem: {                          // Rendered result item            | (Optional)
-        content: (data, source) => {
-            source.innerHTML = data.match;
-        },
-        element: "div"
+    maxResults: 15,
+    highlight: true,
+    resultItem: {
+      content: (data, source) => {
+        source.innerHTML = data.value;
+      },
+      element: "li"
     },
     noResults: (dataFeedback, generateList) => {
-        // Generate autoComplete List
-        generateList(autoCompleteJS, dataFeedback, dataFeedback.results);
-        // No Results List Item
-        const result = document.createElement("li");
-        result.setAttribute("class", "no_result");
-        result.setAttribute("tabindex", "1");
-        result.innerHTML = `<span style="display: flex; align-items: center; font-weight: 100; color: rgba(0,0,0,.2);">Found No Results for "${dataFeedback.query}"</span>`;
-        document.querySelector(`#${autoCompleteJS.resultsList.idName}`).appendChild(result);
+      generateList(window.autoComplete, dataFeedback, dataFeedback.results);
+      const result = document.createElement("li");
+      result.setAttribute("class", "no_result");
+      result.setAttribute("tabindex", "1");
+      result.innerHTML = `<span style="display: flex; align-items: center; font-weight: 100; color: rgba(0,0,0,.2);">Found No Results for "${dataFeedback.query}"</span>`
+      document.querySelector('#autocomplete_list').appendChild(result);
     },
-    onSelection: feedback => {             // Action script onSelection event | (Optional)
-        console.log(feedback.selection.value.image_url);
+    onSelection: feedback => {
+      let postcode = window.getDrivingMatrix(feedback.selection.value);
     }
+  });
 });
+window.getDrivingMatrix = async postcode => {
+  const loader = document.querySelector('#loader');
+  loader.style.display = 'inline-block';
+  const geoDataSource = await fetch(`https://api.postcodes.io/postcodes/${postcode}`);
+  const geoData = await geoDataSource.json();
+  loader.style.display = 'none';
 
-});
+  if (geoData.hasOwnProperty('result') && geoData.result.hasOwnProperty('longitude') && geoData.result.hasOwnProperty('latitude')) {
+    loader.style.display = 'inline-block';
+    const drivingDataSource = await fetch(`/?rest_route=/geomap/v1/lat-lng/${geoData.result.longitude}/${geoData.result.latitude}`);
+  //  const drivingMatrix = await drivingDataSource.json();
+    console.log(drivingDataSource);
+    loader.style.display = 'none';
+    window.localStorage.drivingMatrix = JSON.stringify(drivingDataSource);
+  }
+
+}
